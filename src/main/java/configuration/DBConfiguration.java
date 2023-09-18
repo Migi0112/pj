d3eeaea2882ab61a0399d9a16f1f030c1c16cc09
Migi0112 <mingi52020@gmail.com>
2023-09-18 16:50:22 +0900

    test

--- src/main/java/configuration/DBConfiguration.java
+++ src/main/java/configuration/DBConfiguration.java
@@ -1,44 +1,65 @@
 package configuration;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-// 패키지명
-@MapperScan(value = "com.example.server_01", sqlSessionFactoryRef = "SqlSessionFactory")
+@PropertySource("classpath:/application.properties")
+@EnableTransactionManagement
 public class DBConfiguration {
 
-    @Value("${spring.datasource.mapper-locations}")
-    String mPath;
+    @Autowired
+    private ApplicationContext applicationContext;
 
-    @Bean(name = "dataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource DataSource() {
-        return DataSourceBuilder.create().build();
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
     }
 
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
 
     @Bean(name = "SqlSessionFactory")
-    public SqlSessionFactory SqlSessionFactory(@Qualifier("dataSource") DataSource DataSource, ApplicationContext applicationContext) throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(DataSource);
-        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mPath));
-        return sqlSessionFactoryBean.getObject();
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        //factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/*.xml"));
+        factoryBean.setTypeAliasesPackage("com.team.pj");
+        factoryBean.setConfiguration(mybatisConfg());
+        return factoryBean.getObject();
+    }
+    @Bean
+    public SqlSessionTemplate sqlSession() throws Exception {
+        return new SqlSessionTemplate(sqlSession().getSqlSessionFactory());
     }
 
-    @Bean(name = "SessionTemplate")
-    public SqlSessionTemplate SqlSessionTemplate(@Qualifier("SqlSessionFactory") SqlSessionFactory firstSqlSessionFactory) {
-        return new SqlSessionTemplate(firstSqlSessionFactory);
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration mybatisConfg() {
+        return new org.apache.ibatis.session.Configuration();
+    }
+
+    //tx:annotation-driven 설정-@Transactional를 선언하여 트랜잭션 처리를 할 수 있다.
+    @Bean
+    public PlatformTransactionManager txManager() throws Exception{
+        return new DataSourceTransactionManager(dataSource());
     }
 }

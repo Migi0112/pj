d3ce179aba874d64508dd5384c6917d6b294d5f1
Migi0112 <mingi52020@gmail.com>
2023-10-19 15:58:03 +0900

    test

--- src/main/pjfront/src/App.js
+++ src/main/pjfront/src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
+        <input type="text" value={text} onChange={(e) => {setText(e.target.value);}}/>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          리액트 메인화면
+          리액트 메인화면!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
